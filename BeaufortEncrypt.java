c49a05dfe8b63416961a82a00629ac649232a0e6
Bleron Mujaj <115992187+bleron-mujaj@users.noreply.github.com>
2024-04-19 23:21:37 +0200

    Update BeaufortEncrypt.java

---
+++ BeaufortEncrypt.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BeaufortEncrypt {
+
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String beaufortEncrypt(String text, String key) {
+        StringBuilder cipher = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c0 = text.charAt(i);
+            char c1 = key.charAt(i % key.length());
+            int start = 0;
+            for (int j = 0; j < 26; j++) {
+                if (ALPHABET.charAt(j) == c0) {
+                    start = j;
+                    break;
+                }
+            }
+            int offset = 0;
+            for (int j = start; j < start + 26; j++) {
+                int letter = j % 26;
+                if (ALPHABET.charAt(letter) == c1) {
+                    break;
+                }
+                offset++;
+            }
+            cipher.append(ALPHABET.charAt(offset));
+        }
+        return cipher.toString();
+    }
