cde4b21c0b5cf3b207db970bfa18472d78216363
BleronRrmoku <bleron.rrmoku@student.uni-pr.edu>
2024-04-21 20:13:13 +0200

    Update README.md

--- BeaufortDecrypt.java
+++ BeaufortDecrypt.java
@@ -21,19 +21,18 @@ public class BeaufortDecrypt {
         return decipher.toString();
     }
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Cipher Text: ");
-            String cipher =
-                    scanner.nextLine().toUpperCase();
+            Scanner scanner = new Scanner(System.in); // Krijimi i një objekti të klases Scanner për të pranuar input-in nga përdoruesi
+            System.out.println("Cipher Text: "); //Shfaqet mesazhi per te kerkuar tekstin e koduar
+            String cipher = scanner.nextLine().toUpperCase(); // Merr input-in për tekstin e shifrës dhe e konverton në shkronja të mëdha (Upper Case)
 
-            String key;
-            do { System.out.println("Key:");
-                key = scanner.nextLine().toUpperCase( );
-                if (!isAlpha(key)) {
-                    System.out.println("Error: Celesi duhet te permbaje vetem  shkronja.");
+            String key; // Deklarimi i variablës për ruajtjen e çelësit
+            do { System.out.println("Key:"); // Fillimi i loop-it do-while për siguruar që çelësi i dhënë është vetëm tekst alfanumerik
+                key = scanner.nextLine().toUpperCase( ); // Merr input-in për çelësin dhe e konverton në shkronja të mëdha
+                if (!isAlpha(key)) {  // Kontrolli nëse çelësi i dhënë përmban vetëm shkronja
+                    System.out.println("Error:  çelesi duhet te permbaje vetem  shkronja."); // Shfaqja e një mesazhi gabimi nëse çelësi nuk përmban vetëm shkronja
                 }
-        } while (!isAlpha(key));
-            String message = beaufortDecrypt(cipher, key);
-            System.out.println("Plain Text:" + message);
+        } while (!isAlpha(key)); // Përsërit loop-in do-while derisa të jepet një çelës i vlefshëm
+            String message = beaufortDecrypt(cipher, key); // Dekriptimi i tekstit të shifrës duke përdorur çelësin e dhënë
+            System.out.println("Plain Text:" + message); // Shfaqja e tekstit të dekriptuar të shifrës si output
     }
 }
